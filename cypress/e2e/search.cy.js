19ac4773f85172471b819185567d3d2e650240ad
Isaac Castro Fuentes <isaac@isaaccastro.eu>
2022-08-06 16:55:12 +0200

    Fix e2e test and add e2e script in package.json

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,25 +1,25 @@
 describe('Search', () => {
   beforeEach(() => {
-    cy.intercept('GET', '**/?search=luke', { fixture: 'people/lukeSearchResults.json' })
-    cy.intercept('GET', '**/?search=l', { fixture: 'people/lSearchResults.json' })
+    cy.intercept('GET', '**/people/?search=luke?page=**', { fixture: 'people/lukeSearchResults.json' }).as('getLukeSearchResults')
+    cy.intercept('GET', '**/people/?search=l?page=**', { fixture: 'people/lSearchResults.json' }).as('getLSearchResults')
   })
 
   it('should have 1 record', () => {
     cy.visit('/search?term=luke')
-    cy.wait(500)
+    cy.wait('@getLukeSearchResults')
     cy.get('[class*=itemList]').its('length').should('eq', 1)
   })
 
   it('should have Show more button', () => {
     cy.visit('/search?term=l')
-    cy.wait(500)
+    cy.wait('@getLSearchResults')
     cy.get('[class*=itemList]').its('length').should('eq', 10)
     cy.get('[class=loadMore]').its('length').should('eq', 1)
   })
 
   it('should have 20 records', () => {
     cy.get('[class=loadMore]').click()
-    cy.wait(500)
+    cy.wait('@getLSearchResults')
     cy.get('[class*=itemList]').its('length').should('eq', 20)
   })
   
